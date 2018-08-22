2d4199da5dd957fa2404e4c8097a78fd80a8fc3d
Giorgi Kharshiladze <giorgi.kharshila@gmail.com>
2018-08-22 15:22:31 -0400

    MVC

--- app.py
+++ app.py
@@ -1,54 +1,25 @@
-from bs4 import BeautifulSoup
-import pandas as pd
-import json, requests, datetime
-
-base = 'http://libgen.io/search.php?&phrase=1&open=0&res=100&view=simple&req='
-
-def getHTML(url):
-	response = requests.get(url)
-	source = response.content
-	return source
-
-def getContent(url):
-	html = getHTML(url)
-	soup = BeautifulSoup(html, 'html5lib')
-	return soup
-
-def getInfo(url):
-	content = getContent(url)
-	tables = content.findAll('table')
-	return tables
-
-def getBooks(url):
-	library = getInfo(url)[2]
-	books = library.findAll('tr')[1:]
-	return books
-
-def parseBooks(url):
-	books = getBooks(url)
-	columns = ['author', 'title', 'publisher', 'year', 'pages', 'lang', 'size', 'extension']
-	df = pd.DataFrame(columns=columns)
-
-	for book in books:
-		bookInfo = book.findAll('td')
-		id = bookInfo[0].text
-		author = bookInfo[1].text
-		title = bookInfo[2].text
-		publisher = bookInfo[3].text
-		year = bookInfo[4].text
-		pages = bookInfo[5].text
-		lang = bookInfo[6].text
-		size = bookInfo[7].text
-		extension = bookInfo[8].text
-		mirrors = bookInfo[9:]
-		row = {'author':author, 'title':title, 'publisher':publisher, 'year':year, 'pages':pages, 'lang':lang, 'size':size, 'extension':extension}
-		df = df.append(row, ignore_index=True)
-
-	return df
+from bottle import route, get, post, error, template, run, request, static_file
+import json, helper as hlp
 
 
-keyword = "harry potter"
-keyword_slug = keyword.replace(" ","+")
-my_url = base + keyword_slug
+key = json.load(open('keys.json', 'r'))
 
-print(parseBooks(my_url))
+@route('/')
+def index():
+	return template('index')
+
+
+@post('/search')
+def search():
+	keyword = request.forms.get('keyword')
+	keyword_slug = keyword.replace(" ","+")
+	my_url = key['base'] + keyword_slug
+	return template('results', data=hlp.parseBooks(my_url))
+
+
+# Access static files
+@route('/static/:path#.+#', name='static')
+def static(path):
+    return static_file(path, root='static')
+
+run(host='localhost', port=8080, debug=True, reloader=True)
---
+++ helper.py
@@ -0,0 +1,43 @@
+from bs4 import BeautifulSoup
+import requests, datetime
+
+def getHTML(url):
+	response = requests.get(url)
+	source = response.content
+	return source
+
+def getContent(url):
+	html = getHTML(url)
+	soup = BeautifulSoup(html, 'html5lib')
+	return soup
+
+def getInfo(url):
+	content = getContent(url)
+	tables = content.findAll('table')
+	return tables
+
+def getBooks(url):
+	library = getInfo(url)[2]
+	books = library.findAll('tr')[1:]
+	return books
+
+def parseBooks(url):
+	books = getBooks(url)
+	data = []
+
+	for book in books:
+		bookInfo = book.findAll('td')
+		id = bookInfo[0].text
+		author = bookInfo[1].text
+		title = bookInfo[2].text
+		publisher = bookInfo[3].text
+		year = bookInfo[4].text
+		pages = bookInfo[5].text
+		lang = bookInfo[6].text
+		size = bookInfo[7].text
+		extension = bookInfo[8].text
+		mirrors = bookInfo[9:]
+		row = {'author':author, 'title':title, 'publisher':publisher, 'year':year, 'pages':pages, 'lang':lang, 'size':size, 'extension':extension}
+		data.append(row)
+
+	return data
